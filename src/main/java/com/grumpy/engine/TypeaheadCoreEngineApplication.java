4c8c053c910e4795aa68ece969d26fcf5a22282f
Saumya Srivastava <saumyasrivastava22@gmail.com>
2021-10-26 19:33:26 +0530

    project setup, initial commit.

---
+++ src/main/java/com/grumpy/engine/TypeaheadCoreEngineApplication.java
@@ -0,0 +1,13 @@
+package com.grumpy.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeaheadCoreEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeaheadCoreEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grumpy/engine/TypeaheadCoreEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grumpy.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeaheadCoreEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
